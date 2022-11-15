be818c7f46bae8a23017b6591d7ae891d7df1d59
flowjiyun <gyp2795@gmail.com>
2022-11-15 17:35:19 +0900

    test: test parser && check syntax error && sytax && check tree traverse

--- include/test.h
+++ include/test.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cheseo <cheseo@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 14:46:46 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/11/11 16:15:22 by cheseo           ###   ########.fr       */
+/*   Updated: 2022/11/15 17:01:10 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,8 @@ void	test_is_pair_quote(void);
 void	test_is_pair_bracket(void);
 void	test_is_correct_pair(void);
 void	test_read_wrapped_content(void);
-void    test_tokenize(void);
+void	test_tokenize(void);
+void	test_parser(void);
+void	test_check_syntax_error(void);
+
 #endif
