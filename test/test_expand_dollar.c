2d1b30aafc572a4d5e1d22155252b440076055ed
flowjiyun <gyp2795@gmail.com>
2022-11-17 17:26:25 +0900

    test: test expand dollar

--- test/test_expand_dollar.c
+++ test/test_expand_dollar.c
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 17:00:06 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/11/17 09:08:36 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2022/11/17 17:14:52 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,27 +25,31 @@ static t_token	*make_token(char *value, int priority)
 void	test_expand_dollar(void)
 {
 	t_token	*token1;
-	// t_token	*token2;
-	// t_token	*token3;
-	// t_token	*token4;
-	// t_token	*token5;
+	t_token	*token2;
+	t_token	*token3;
+	t_token	*token4;
+	t_token	*token5;
 
-	printf("\n=======echo \"dfjk\"df\"$P\"sld\"fkj\"=================\n");
-	token1 = make_token("\"dfjk\"df\"$P\"sld\"fkj\"", 2);
+	printf("\n=======\"$PATH\"=================\n");
+	token1 = make_token("\"$PATH\"", 2);
 	expand_dollar(token1);
 	printf("%s\n", token1->value);
 	printf("\n==============================================\n");
-	// token2 = make_token("*", 2);
-	// expand_dollar(token2);
-	// printf("%s\n", token2->value);
-	// printf("\n==============================================\n");
-	// token3 = make_token("\"t*s*\"", 2);
-	// expand_dollar(token3);
-	// printf("%s\n", token3->value);
-	// printf("\n==============================================\n");
-	// token4 = make_token(".h*", 2);
-	// expand_dollar(token4);
-	// printf("%s\n", token4->value);
+	token2 = make_token("\"hello\"$USER\"hello\"", 2);
+	expand_dollar(token2);
+	printf("%s\n", token2->value);
+	printf("\n==============================================\n");
+	token3 = make_token("\"hello hello $PATHhello $PATH hello\"", 2);
+	expand_dollar(token3);
+	printf("%s\n", token3->value);
+	printf("\n==============================================\n");
+	token4 = make_token("\"hello hello \"$PATH\"hello hello\"", 2);
+	expand_dollar(token4);
+	printf("%s\n", token4->value);
+	printf("\n==============================================\n");
+	token5 = make_token("$PATH$USER'$USER'''$USER''", 2);
+	expand_dollar(token5);
+	printf("%s\n", token5->value);
 	// printf("\n==============================================\n");
 	// token5 = make_token("*kefile", 2);
 	// expand_dollar(token5);
