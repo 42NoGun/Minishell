e6e0336382e5f0de2286553228ba6adce18f53d9
flowjiyun <gyp2795@gmail.com>
2022-11-17 17:25:26 +0900

    feat: add export logic

---
+++ src/built_in/export.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+//#include "libft.h"
+
+// int main(int argc, char **argv, char *envp[])
+// {
+//     (void) argc;
+//     char *value;
+//     char **init_pos = envp;
+//     value = getenv(argv[1]);
+
+
+//    // if (value != NULL)
+//     // {
+//     //     printf("%s\n", value);
+//     // }
+//     while(*envp)
+//     {
+//         envp++;
+//     }
+//     *envp = ft_strdup("strdup=hello");
+//     envp++;
+//     *envp = NULL;
+
+//     while(*init_pos)
+//     {
+//         init_pos++;
+//         printf("%s\n", *init_pos);
+//     }
+//     printf("============\n");
+//     printf("usr_env : %s\n", getenv("strdup"));
+
+//     return 0;
+// }
+// fi
+// $확장 먼저
+// wildcard 확 장 먼저 
