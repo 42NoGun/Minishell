fec34e1a71c495cdb8041603edae6a46f1ad0bc4
flowjiyun <gyp2795@gmail.com>
2022-12-01 17:16:31 +0900

    fix: human error(ft_strncmp return value)

--- src/expansion/expand.c
+++ src/expansion/expand.c
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:31:58 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/11/29 13:43:02 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2022/12/01 16:41:58 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -147,7 +147,7 @@ char	*expand_content(char *content)
 			++env_content_len;
 		}
 		env_content = ft_substr(content - env_content_len, 0, env_content_len);
-		if (ft_strcmp(env_content, "?"))
+		if (ft_strcmp(env_content, "?") == 0)
 		{
 			converted_env_content = ft_itoa(g_exit_status);
 			expanded_content = ft_strjoin(expanded_content,
