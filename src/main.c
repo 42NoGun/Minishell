1e7582eb17c82fd613373f96352b91d79ada233d
flowjiyun <gyp2795@gmail.com>
2022-12-07 18:01:20 +0900

    feat: execute empty line command

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 11:43:12 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/12/06 15:55:52 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2022/12/07 16:16:53 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,10 +52,15 @@ int main(int argc, char **argv, char **envp)
 		line = readline("minishell > ");
 		if (!line)
 		{
-			printf("\033[1A");
-			printf("\033[13C");
+			// printf("\033[1A");
+			// printf("\033[13C");
 			printf("exit\n");
 			exit(0);
+		}
+		if (ft_strcmp(line, "") == 0)
+		{
+			free(line);
+			continue ;
 		}
 		if (*line)
 			add_history(line);
