17d2faf38d81458127914f2fd9cb7d1080b234d2
flowjiyun <gyp2795@gmail.com>
2022-12-08 18:03:24 +0900

    feat: add subshell logic

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 11:43:12 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/12/07 16:16:53 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2022/12/08 16:26:43 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,58 @@
 #include <errno.h>
 #include "minishell.h"
 
-// void	subshell_logic(int argc, char **argv, char **envp)
-// {
-// 	int	i;
+int i;
+void	subshell_logic(int argc, char **argv, char **envp)
+{
+	int	i;
 
-// 	char *line;
-// 	line = ft_strjoin(argv[0]);
-// 	tokenize(line, cmd_list); // parser 안으로
-// 	// syntax_check(cmd_list);
-// 	parser(cmd_tree, cmd_list);
-// 	check_syntax_error(cmd_tree); // parser 안으로
-// 	free(line);
-// }
+	t_list *cmd_list;
+	t_tree *cmd_tree;
+	char *line;
+
+	cmd_list = init_list();
+	cmd_tree = init_tree();
+	line = ft_strdup(argv[1]);
+	if (ft_strcmp(line, "") == 0)
+	{
+		free(line);
+		return ;
+	}
+	if (is_correct_pair(line) == false)
+	{
+		free(line);
+		return ;
+	}
+	tokenize(line, cmd_list);
+	parser(cmd_tree, cmd_list);
+	if (check_syntax_error(cmd_tree) == false)
+	{
+		free(line);
+		return ;
+	}
+	execute(convert_tree_to_exec_list(cmd_tree), envp);
+	free(line);
+}
+
+void	hello(char *out)
+{
+	printf("\n");
+	if (rl_on_new_line() == -1)
+		exit(1);
+    rl_replace_line("", 10);
+   	rl_redisplay();
+}
+void	signal_interrupt(int signum)
+{
+	g_exit_status = 130 << 8;
+	hello(NULL);
+}
+
+void	define_signal(void)
+{
+	signal(SIGINT, signal_interrupt);
+	signal(SIGQUIT, SIG_IGN);
+}
 
 int main(int argc, char **argv, char **envp)
 {
@@ -40,16 +80,21 @@ int main(int argc, char **argv, char **envp)
 	t_tree *cmd_tree;
 
 	errno = 0;
-	// if (argc > 2)
-	// {
-	// 	subshell_logic(argc, argv, envp);
-	// 	return (0);
-	// }
+	define_signal();
+	if (argc >= 2)
+	{
+		subshell_logic(argc, argv, envp);
+		return (WEXITSTATUS(g_exit_status));
+	}
 	while (1)
 	{
+		i = 1;
 		cmd_list = init_list();
 		cmd_tree = init_tree();
+
+		//hello("minishell > "); 
 		line = readline("minishell > ");
+		i = 0;
 		if (!line)
 		{
 			// printf("\033[1A");
@@ -60,6 +105,7 @@ int main(int argc, char **argv, char **envp)
 		if (ft_strcmp(line, "") == 0)
 		{
 			free(line);
+			// g_exit_status = 127
 			continue ;
 		}
 		if (*line)
