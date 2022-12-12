2b9c25dc2585c09bd2f1a8f43de85d679c27c2ce
flowjiyun <gyp2795@gmail.com>
2022-12-12 17:52:56 +0900

    feat: add return exitstatus logic

--- src/built_in/exit.c
+++ src/built_in/exit.c
@@ -6,13 +6,28 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 13:53:25 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/12/08 13:53:46 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2022/12/12 14:52:08 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	b_exit(void)
+void	b_exit(char **command)
 {
-	exit(0);
+	int	i;
+
+	ft_putendl_fd("exit", 2);
+	i = 0;
+	while (command[i])
+		i++;
+	if (i > 2)
+	{
+		ft_putstr_fd("exit: ", 2);
+		ft_putendl_fd("Too many arguments", 2);
+		g_exit_status = 1 << 8;
+		return ;
+	}
+	if (command[1] == NULL)
+		exit(0);
+	exit(ft_atoi(command[1]));
 }
