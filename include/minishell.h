e9e082493359590c90d318786ee677094225a607
flowjiyun <gyp2795@gmail.com>
2022-12-12 17:56:08 +0900

    feat: add bulit_in functions

--- include/minishell.h
+++ include/minishell.h
@@ -69,6 +69,8 @@ bool	is_pair_quote(const char *line);
 
 char	*ft_charjoin(char *str, char c);
 char	*ft_chardup(char c);
+char	*ft_getenv(t_list *env_list, char *env);
+void	ft_setenv(t_list *env_list, char *key, char *command);
 
 int		get_priority(char *prev_str);
 t_token	*create_token(char *prev_str);
@@ -77,17 +79,19 @@ bool	parser(t_tree *cmd_tree, t_list *cmd_list);
 bool	check_syntax_error(t_tree *cmd_tree);
 bool	is_matchable_asterisk(char *dst_file, char *src_file);
 void	expand_wildcard(t_token *token);
-void	expand_dollar(t_token *token);
+void	expand_dollar(t_token *token, t_list *env_list);
 t_list	*convert_tree_to_exec_list(t_tree *cmd_tree);
 void	remove_quote(t_token *token);
-void	expand_field(t_field *field);
+void	expand_field(t_field *field, t_list *env_list);
 void	refine_field(t_field *field, char ***command, char ***redirections);
-void	execute(t_list *exec_list, char **envp);
+void	execute(t_list *exec_list, t_list *env_list);
 
-void	b_exit(void);
 void	b_pwd(void);
-void	b_cd(char **command);
+void	b_cd(char **command, t_list *env_list);
 void	b_echo(char **commands);
+void	b_env(char **command, t_list *env_list);
+void	b_exit(char **command);
+void	b_export(char **command, t_list *env_list);
 
 
 void	free_token(t_token *token);
