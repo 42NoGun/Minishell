9a8472135a04f8c9501aacd31a58c027c837ceb3
Chewon Seo <cheseo@c2r5s3.42seoul.kr>
2022-12-20 11:52:44 +0900

    norm: heredoc, make heredoc_utils.c

--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: cheseo <cheseo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 17:18:29 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/12/16 17:22:41 by jiyunpar         ###   ########.fr       */
+/*   Updated: 2022/12/20 11:51:47 by cheseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,24 @@
 #include <readline/readline.h>
 #include <fcntl.h>
 
-void	skip_quote_content(char **value, char quote)
+void	count_subshell_heredoc(
+	char **value, int *heredoc_count, t_list **limiter_list)
 {
-	size_t	len;
-
-	len = ft_strchr((*value + 1), quote) - *value;
-	*value += len;
-}
-
-void	push_back_limiter(t_node *node, t_list *limiter_list)
-{
-	char	*value;
-
-	value = ((t_token *)node->content)->value;
-	push_back(limiter_list, make_node(ft_strdup(value)));
-}
-
-char	*get_limiter(char *value)
-{
-	int	end;
-
-	end = 0;
-	while (value[end])
+	while (**value)
 	{
-		if (!ft_isalnum(value[end]))
-			break ;
-		++end;
+		if (**value == '"' || **value == '\'')
+			skip_quote_content(value, **value);
+		else
+		{
+			if (**value == '<' && *(*value + 1) == '<')
+			{
+				++*heredoc_count;
+				++(*value);
+				push_back_subshell_limiter(*value, *limiter_list);
+			}
+		}
+		++(*value);
 	}
-	return (ft_substr(value, 0, end));
-}
-
-void	push_back_subshell_limiter(char *value, t_list *limiter_list)
-{
-	char	*limiter;
-
-	++value;
-	while (*value && *value == ' ')
-		++value;
-	limiter = get_limiter(value);
-	push_back(limiter_list, make_node(limiter));
 }
 
 int	count_heredoc(t_field *field, int field_len, t_list *limiter_list)
@@ -68,23 +47,7 @@ int	count_heredoc(t_field *field, int field_len, t_list *limiter_list)
 	{
 		value = ((t_token *)cur_node->content)->value;
 		if (value[0] == '(')
-		{		
-			while (*value)
-			{
-				if (*value == '"' || *value == '\'')
-					skip_quote_content(&value, *value);
-				else
-				{
-					if (*value == '<' && *(value + 1) == '<')
-					{
-						++heredoc_count;
-						++value; // 커서가 두번째 <에 있어요
-						push_back_subshell_limiter(value, limiter_list);
-					}
-				}
-				++value;
-			}
-		}
+			count_subshell_heredoc(&value, &heredoc_count, &limiter_list);
 		else
 		{
 			if (ft_strcmp(value, "<<") == 0)
@@ -99,7 +62,7 @@ int	count_heredoc(t_field *field, int field_len, t_list *limiter_list)
 	return (heredoc_count);
 }
 
-int check_heredoc(t_list *exec_list, t_list *limiter_list)
+int	check_heredoc(t_list *exec_list, t_list *limiter_list)
 {
 	t_node		*cur_exec_node;
 	t_field		*field;
@@ -122,7 +85,6 @@ void	write_heredoc(char *file_path, t_list *limiter_list)
 	int		i;
 	char	*line;
 	char	*limiter;
-
 
 	limiter = (limiter_list->head->content);
 	pop_front(limiter_list);
@@ -150,10 +112,8 @@ bool	make_heredoc_file(t_list *exec_list)
 {
 	int			count;
 	int			i;
-	int			fd;
 	char		*current_file_sequence;
 	char		*current_file_path;
-	const char	*file_name_prefix = "/tmp/heredoc/";
 	t_list		*limiter_list;
 
 	i = 0;
@@ -162,7 +122,8 @@ bool	make_heredoc_file(t_list *exec_list)
 	while (i < count)
 	{
 		current_file_sequence = ft_itoa(i);
-		current_file_path = ft_strjoin_right_free(file_name_prefix, current_file_sequence);
+		current_file_path
+			= ft_strjoin_right_free("/tmp/heredoc/", current_file_sequence);
 		write_heredoc(current_file_path, limiter_list);
 		++i;
 	}
