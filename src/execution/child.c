5570e3489176bcd504bda634a9a736f856c939c4
flowjiyun <gyp2795@gmail.com>
2022-12-22 17:09:59 +0900

    refactor: execute.c

---
+++ src/execution/child.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   child.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/22 16:49:29 by jiyunpar          #+#    #+#             */
+/*   Updated: 2022/12/22 16:59:42 by jiyunpar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/stat.h>
+#include "minishell.h"
+
+static char	**put_program_name(char **old_command)
+{
+	char	**new_command;
+	int		i;
+
+	i = 0;
+	while (old_command[i])
+		++i;
+	new_command = ft_calloc(sizeof(char *), i + 2);
+	new_command[0] = ft_strdup("./minishell");
+	i = 1;
+	while (old_command[i - 1])
+	{
+		new_command[i] = ft_strdup(old_command[i - 1]);
+		++i;
+	}
+	i = 0;
+	while (old_command[i])
+	{
+		free(old_command[i]);
+		++i;
+	}
+	free(old_command);
+	return (new_command);
+}
+
+static bool	put_correct_path(char *path, char **command, struct stat *buf)
+{
+	char	*command_with_path;
+
+	command_with_path = ft_strjoin_left_free(path, "/");
+	command_with_path = ft_strjoin_left_free(command_with_path, *command);
+	if (lstat(command_with_path, buf) == 0)
+	{
+		free(*command);
+		*command = command_with_path;
+		return (true);
+	}
+	return (false);
+}
+
+static void	find_path(char **command, t_list *env_list)
+{
+	char		**path_list;
+	int			i;
+	struct stat	buf;
+
+	if (ft_strchr(*command, '/') != NULL)
+	{
+		if (lstat(*command, &buf) != 0)
+			*command = NULL;
+		return ;
+	}
+	path_list = ft_split(ft_getenv(env_list, "PATH"), ':');
+	i = 0;
+	while (path_list[i])
+	{
+		if (put_correct_path(path_list[i], command, &buf))
+			return ;
+		++i;
+	}
+	*command = NULL;
+}
+
+static void	set_pipe(t_pipe *p, t_context *c)
+{
+	if (p->prev_pipe_in != -1)
+	{
+		_dup2(p->prev_pipe_in, 0);
+		_close(p->prev_pipe_in);
+	}
+	heredoc(c->redirections, p->std_in, false);
+	if (p->has_pipe)
+	{
+		_dup2(p->fd_pipe[1], 1);
+		_close(p->fd_pipe[1]);
+		_close(p->fd_pipe[0]);
+		redirection(c->redirections, false);
+	}
+	else
+		redirection(c->redirections, false);
+}
+
+static void	set_program_path(t_context *c, t_list *env_list, bool is_subshell)
+{
+	if (is_subshell)
+	{
+		remove_bracket(*c->command);
+		c->command = put_program_name(c->command);
+	}
+	else
+		find_path(c->command, env_list);
+}
+
+pid_t	do_child_process(t_context *c, t_list *env_list,
+	bool is_subshell, t_pipe *p)
+{
+	pid_t	pid;
+
+	pid = _fork();
+	if (pid != 0)
+		return (pid);
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	if (*c->command == NULL)
+		exit(0);
+	set_pipe(p, c);
+	if (is_builtin(*c->command) == true)
+	{
+		do_builtin(c->command, env_list, false);
+		exit(0);
+	}
+	set_program_path(c, env_list, is_subshell);
+	if (execve(*c->command, c->command, list_to_2d_array(env_list)) == -1)
+	{
+		_write(2, "command not found\n", 19);
+		exit(127);
+	}
+	return (pid);
+}
--- src/execution/execute.c
+++ src/execution/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cjeseo <cjiyunpeseo@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 11:30:18 by hanbkim           #+#    #+#             */
-/*   Updated: 2022/12/21 10:42:20 by cjiyunpeseo          ###   ########.fr       */
+/*   Updated: 2022/12/22 16:59:37 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,14 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 #include "minishell.h"
+
+bool	is_pipe(char *cmd)
+{
+	if (ft_strcmp(cmd, "|") == 0)
+		return (true);
+	return (false);
+}
 
 bool	is_logical_and(char *cmd)
 {
@@ -30,22 +36,6 @@ bool	is_logical_or(char *cmd)
 	if (ft_strcmp(cmd, "||") == 0)
 		return (true);
 	return (false);
-}
-
-void	skip_node_to_logical_operator(t_node **cur_node)
-{
-	t_field	*cur_field;
-	t_token	*start_token;
-
-	while (*cur_node)
-	{	
-		cur_field = (t_field *)((*cur_node)->content);
-		start_token = (t_token *)(cur_field->start_ptr->content);
-		if (is_logical_and(start_token->value)
-			|| is_logical_or(start_token->value))
-			break ;
-		(*cur_node) = (*cur_node)->next;
-	}
 }
 
 bool	is_builtin(char	*command)
@@ -69,18 +59,6 @@ bool	is_builtin(char	*command)
 	return (false);
 }
 
-bool	pipe_connect(t_node *cur_next_node, int fd_pipe[2])
-{
-	t_token	*token;
-
-	if (cur_next_node == NULL)
-		return (false);
-	token = (t_token *)((t_field *)cur_next_node->content)->start_ptr->content;
-	if (ft_strcmp(token->value, "|") != 0)
-		return (false);
-	_pipe(&fd_pipe);
-	return (true);
-}
 
 bool	is_ordered_heredoc(char **file_path, char *sequence)
 {
@@ -89,7 +67,7 @@ bool	is_ordered_heredoc(char **file_path, char *sequence)
 	int				i;
 
 	i = 0;
-	dirp = opendir("/tmp/heredoc/"); // error 처리 필요 
+	dirp = opendir("/tmp/heredoc/");
 	dp = readdir(dirp);
 	dp = readdir(dirp);
 	while (true)
@@ -155,12 +133,12 @@ bool	heredoc(char **redirections, int std_in, bool parent)
 	{	
 		if (ft_strcmp(redirections[i], "<<") == 0)
 		{
-			dup2(std_in, 0);
+			_dup2(std_in, 0);
 			file_path = get_heredoc_file_path();
 			fd = open(file_path, O_RDONLY);
 			if (open_error(fd, parent) == true)
 				return (false);
-			dup2(fd, 0);
+			_dup2(fd, 0);
 			_close(fd);
 			unlink(file_path);
 			free(file_path);
@@ -172,48 +150,59 @@ bool	heredoc(char **redirections, int std_in, bool parent)
 	return (true);
 }
 
-bool	redirection(char **redirections, bool parent)
+static bool	redirect_append_outstream(char *file, bool parent)
 {
-	int	i;
 	int	fd;
 
+	fd = open(file, O_CREAT | O_WRONLY | O_APPEND, 0666);
+	if (open_error(fd, parent))
+		return (true);
+	_dup2(fd, 1);
+	_close(fd);
+	return (false);
+}
+
+static bool	redirect_outstream(char *file, bool parent)
+{
+	int	fd;
+
+	fd = open(file, O_CREAT | O_TRUNC | O_WRONLY, 0666);
+	if (open_error(fd, parent))
+		return (true);
+	_dup2(fd, 1);
+	_close(fd);
+	return (false);
+}
+
+static bool	redirect_instream(char *file, bool parent)
+{
+	int	fd;
+
+	fd = open(file, O_RDONLY, 0666);
+	if (open_error(fd, parent))
+		return (true);
+	_dup2(fd, 0);
+	_close(fd);
+	return (false);
+}
+
+bool	redirection(char **redirections, bool parent)
+{
+	int		i;
+	bool	open_error;
+
 	i = 0;
+	open_error = false;
 	while (redirections[i])
 	{
 		if (ft_strcmp(redirections[i], ">>") == 0)
-		{
-			fd = open(redirections[i + 1], O_CREAT | O_WRONLY | O_APPEND, 0666);
-			if (fd == -1)
-				ft_terminate("redirection, open");
-			dup2(fd, 1);
-			_close(fd);
-		}
-		else if (ft_strcmp(redirections[i], "<") == 0)
-		{
-			fd = open(redirections[i + 1], O_RDONLY);
-			if (fd == -1 && !parent)
-				ft_terminate("redirection, open");
-			if (fd == -1 && parent)
-			{
-				ft_putendl_fd(strerror(errno), 2);
-				return (false);
-			}
-			dup2(fd, 0);
-			_close(fd);
-		}
-		else if (ft_strcmp(redirections[i], ">") == 0)
-		{
-			fd = open(redirections[i + 1], O_CREAT | O_TRUNC | O_WRONLY, 0666);
-			if (fd == -1 && !parent)
-				ft_terminate("redirection, open");
-			if (fd == -1 && parent)
-			{
-				ft_putendl_fd(strerror(errno), 2);
-				return (false);
-			}
-			dup2(fd, 1);
-			_close(fd);
-		}
+			open_error = redirect_append_outstream(redirections[i + 1], parent);
+		if (ft_strcmp(redirections[i], ">") == 0)
+			open_error = redirect_outstream(redirections[i + 1], parent);
+		if (ft_strcmp(redirections[i], "<") == 0)
+			open_error = redirect_instream(redirections[i + 1], parent);
+		if (open_error)
+			return (false);
 		i += 2;
 	}
 	return (true);
@@ -225,31 +214,7 @@ void	remove_bracket(char *command)
 	command[ft_strlen(command) - 1] = 0;
 }
 
-char	**put_program_name(char **old_command)
-{
-	char	**new_command;
-	int		i;
 
-	i = 0;
-	while (old_command[i])
-		++i;
-	new_command = ft_calloc(sizeof(char *), i + 2);
-	new_command[0] = ft_strdup("./minishell");
-	i = 1;
-	while (old_command[i - 1])
-	{
-		new_command[i] = ft_strdup(old_command[i - 1]);
-		++i;
-	}
-	i = 0;
-	while (old_command[i])
-	{
-		free(old_command[i]);
-		++i;
-	}
-	free(old_command);
-	return (new_command);
-}
 
 void	do_builtin(char **command, t_list *env_list, bool parent)
 {
@@ -267,54 +232,6 @@ void	do_builtin(char **command, t_list *env_list, bool parent)
 		g_exit_status = b_env(command, env_list) << 8;
 	else if (ft_strcmp(*command, "exit") == 0)
 		g_exit_status = b_exit(command, parent) << 8;
-}
-
-void	find_path(char **command, t_list *env_list)
-{
-	char		**path_list;
-	int			i;
-	char		*command_with_path;
-	struct stat	buf;
-
-	if (ft_strchr(*command, '/') != NULL)
-	{
-		if (lstat(*command, &buf) != 0)
-			*command = NULL;
-		return ;
-	}
-	path_list = ft_split(ft_getenv(env_list, "PATH"), ':');
-	i = 0;
-	while (path_list[i])
-	{
-		command_with_path = ft_strjoin_left_free(path_list[i], "/");
-		command_with_path = ft_strjoin_left_free(command_with_path, *command);
-		if (lstat(command_with_path, &buf) == 0)
-		{
-			free(*command);
-			*command = command_with_path;
-			return ;
-		}
-		++i;
-	}
-	*command = NULL;
-}
-
-char	**list_to_2d_array(t_list *envp_list)
-{
-	char	**arr;
-	int		i;
-	t_node	*cur_node;
-
-	arr = ft_calloc(sizeof(char *), envp_list->len + 1);
-	i = 0;
-	cur_node = envp_list->head;
-	while (cur_node)
-	{
-		arr[i] = (char *) cur_node->content;
-		++i;
-		cur_node = cur_node->next;
-	}
-	return (arr);
 }
 
 void	expand_field(t_field *field, t_list *env_list, bool is_subshell)
@@ -337,152 +254,106 @@ void	expand_field(t_field *field, t_list *env_list, bool is_subshell)
 	}
 }
 
-void	execute(t_list *exec_list, t_list *env_list)
+void	skip_node_to_logical_operator(t_node **cur_node)
 {
-	t_field	*field;
-	t_token	*token;
-	t_node	*cur_node;
-	t_node	*cur_next_node;
-	char	**command;
-	char	**redirections;
-	int		fd_pipe[2];
-	int		prev_pipe_in;
-	bool	has_pipe;
-	t_list	*pid_list;
-	pid_t	pid;
-	int		std_in;
-	int		std_out;
-	bool	is_subshell;
+	t_field	*cur_field;
+	t_token	*start_token;
 
-	pid_list = init_list();
-	prev_pipe_in = -1;
-	cur_node = exec_list->head;
-	std_in = dup(0);
-	std_out = dup(1);
-	while (cur_node)
-	{
-		is_subshell = false;
-		cur_next_node = cur_node->next;
-		field = (t_field *) cur_node->content;
-		token = (t_token *)(field->start_ptr->content);
-		if (token->value[0] == '(')
-			is_subshell = true;
-		if (ft_strcmp(token->value, "|") == 0)
-		{
-			cur_node = cur_node->next;
-			continue ;
-		}
-		else if (is_logical_and(token->value))
-		{	
-			prev_pipe_in = -1;
-			waitpid(pid, &g_exit_status, 0);
-			if (WIFSIGNALED(g_exit_status) == true)
-			{
-				cur_node = NULL;
-				continue ;
-			}
-			if (g_exit_status == 0)
-			{
-				cur_node = cur_node->next;
-				continue ;
-			}
-			else
-			{
-				cur_node = cur_node->next;
-				skip_node_to_logical_operator(&cur_node);
-				continue ;
-			}
-		}
-		else if (is_logical_or(token->value))
-		{
-			prev_pipe_in = -1;
-			waitpid(pid, &g_exit_status, 0);
-			if (WIFSIGNALED(g_exit_status) == true)
-			{
-				cur_node = NULL;
-				continue ;
-			}
-			if (g_exit_status == 0)
-			{
-				cur_node = cur_node->next;
-				skip_node_to_logical_operator(&cur_node);
-				continue ;
-			}
-			else
-			{
-				cur_node = cur_node->next;
-				continue ;
-			}
-		}	
-		has_pipe = pipe_connect(cur_next_node, fd_pipe);
-		expand_field(field, env_list, is_subshell);
-		refine_field(field, &command, &redirections, 0);
-		if (has_pipe == false && is_builtin(*command) && prev_pipe_in == -1)
-		{
-			if (heredoc(redirections, std_in, true) == true
-				&& redirection(redirections, true) == true)
-			{
-				do_builtin(command, env_list, true);
-				dup2(std_in, 0);
-				dup2(std_out, 1);
-			}
-			cur_node = cur_node->next;
-			free_2d_str(command);
-			free_2d_str(redirections);
-			continue ;
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			signal(SIGINT, SIG_DFL);
-			signal(SIGQUIT, SIG_DFL);
-			if (prev_pipe_in != -1)
-			{
-				dup2(prev_pipe_in, 0);
-				_close(prev_pipe_in); // 여기
-			}
-			heredoc(redirections, std_in, false);
-			if (has_pipe)
-			{
-				dup2(fd_pipe[1], 1);
-				_close(fd_pipe[1]);
-				_close(fd_pipe[0]);
-				redirection(redirections, false);
-			}
-			else
-				redirection(redirections, false);
-			if (*command == NULL)
-				exit(0);
-			if (is_subshell)
-			{
-				remove_bracket(*command);
-				command = put_program_name(command);
-			}
-			else if (is_builtin(*command) == true)
-			{
-				do_builtin(command, env_list, false);
-				exit(0);
-			}
-			else
-				find_path(command, env_list);
-			if (execve(command[0], command, list_to_2d_array(env_list)) == -1)
-			{
-				_write(2, "command not found\n", 19);
-				exit(127);
-			}
-		}
-		push_back(pid_list, make_node((void *)(long long)pid));
-		if (prev_pipe_in != -1)
-			_close(prev_pipe_in); // 여기
-		if (has_pipe)
-		{
-			prev_pipe_in = fd_pipe[0];
-			_close(fd_pipe[1]);
-		}
-		cur_node = cur_node->next;
-		free_2d_str(command);
-		free_2d_str(redirections);
+	while (*cur_node)
+	{	
+		cur_field = (t_field *)((*cur_node)->content);
+		start_token = (t_token *)(cur_field->start_ptr->content);
+		if (is_logical_and(start_token->value)
+			|| is_logical_or(start_token->value))
+			break ;
+		(*cur_node) = (*cur_node)->next;
 	}
+}
+
+bool	do_logical_operator(t_node **cur_node, char *token,
+	int *prev_pipe_in, pid_t pid)
+{
+	if (is_logical_and(token) || is_logical_or(token))
+	{
+		*prev_pipe_in = -1;
+		waitpid(pid, &g_exit_status, 0);
+		if (WIFSIGNALED(g_exit_status) == true)
+		{
+			*cur_node = NULL;
+			return (true);
+		}
+		*cur_node = (*cur_node)->next;
+		if (g_exit_status != 0 && is_logical_and(token))
+			skip_node_to_logical_operator(cur_node);
+		if (g_exit_status == 0 && is_logical_or(token))
+			skip_node_to_logical_operator(cur_node);
+		return (true);
+	}
+	return (false);
+}
+
+bool	do_pipe(t_node **cur_node, char *token)
+{
+	if (is_pipe(token))
+	{
+		*cur_node = (*cur_node)->next;
+		return (true);
+	}
+	return (false);
+}
+
+bool	do_parent_builtin(t_node **cur_node, t_list *env_list,
+	t_context *c, t_pipe *p)
+{
+	if (p->has_pipe == false && is_builtin(*c->command)
+		&& p->prev_pipe_in == -1)
+	{
+		if (heredoc(c->redirections, p->std_in, true) == true
+			&& redirection(c->redirections, true) == true)
+		{
+			do_builtin(c->command, env_list, true);
+			_dup2(p->std_in, 0);
+			_dup2(p->std_out, 1);
+		}
+		*cur_node = (*cur_node)->next;
+		free_2d_str(c->command);
+		free_2d_str(c->redirections);
+		return (true);
+	}
+	return (false);
+}
+
+void	do_parent_process(t_node **cur_node, t_context *c,
+	t_pipe *p, t_pid_utils *process)
+{
+	push_back(process->pid_list, make_node((void *)(long long)process->pid));
+	if (p->prev_pipe_in != -1)
+		_close(p->prev_pipe_in);
+	if (p->has_pipe)
+	{
+		p->prev_pipe_in = p->fd_pipe[0];
+		_close(p->fd_pipe[1]);
+	}
+	*cur_node = (*cur_node)->next;
+	free_2d_str(c->command);
+	free_2d_str(c->redirections);
+}
+
+bool	pipe_connect(t_node *cur_next_node, t_pipe *p)
+{
+	t_token	*token;
+
+	if (cur_next_node == NULL)
+		return (false);
+	token = (t_token *)((t_field *)cur_next_node->content)->start_ptr->content;
+	if (ft_strcmp(token->value, "|") != 0)
+		return (false);
+	_pipe(p->fd_pipe);
+	return (true);
+}
+
+static void	wait_child_process(t_list *pid_list, int std_in, int std_out)
+{
 	while (pid_list->len)
 	{
 		waitpid((pid_t) pid_list->head->content, &g_exit_status, 0);
@@ -503,4 +374,41 @@ void	execute(t_list *exec_list, t_list *env_list)
 	free_list_only_node(pid_list);
 	_close(std_in);
 	_close(std_out);
+}
+
+static void	init_execute_utils(t_pipe *pipe_utils, t_pid_utils *process)
+{
+	pipe_utils->prev_pipe_in = -1;
+	pipe_utils->std_in = dup(0);
+	pipe_utils->std_out = dup(1);
+	process->pid_list = init_list();
+}
+
+void	execute(t_list *exec_list, t_list *env_list)
+{
+	t_node		*cur_node;
+	t_context	context;
+	t_pipe		pipe_utils;
+	t_pid_utils	process;
+
+	init_execute_utils(&pipe_utils, &process);
+	cur_node = exec_list->head;
+	while (cur_node)
+	{
+		if (do_pipe(&cur_node, get_value(cur_node)))
+			continue ;
+		if (do_logical_operator(&cur_node,
+				get_value(cur_node), &(pipe_utils.prev_pipe_in), process.pid))
+			continue ;
+		pipe_utils.has_pipe = pipe_connect(cur_node->next, &pipe_utils);
+		expand_field(get_field(cur_node), env_list, is_subshell(cur_node));
+		refine_field(get_field(cur_node),
+			&context.command, &context.redirections);
+		if (do_parent_builtin(&cur_node, env_list, &context, &pipe_utils))
+			continue ;
+		process.pid = do_child_process(&context,
+				env_list, is_subshell(cur_node), &pipe_utils);
+		do_parent_process(&cur_node, &context, &pipe_utils, &process);
+	}
+	wait_child_process(process.pid_list, pipe_utils.std_in, pipe_utils.std_out);
 }
