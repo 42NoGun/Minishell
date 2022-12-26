2060af510b2c5cb5e8e3cd2a0245943f2bf12814
flowjiyun <gyp2795@gmail.com>
2022-12-26 15:14:12 +0900

    fix: exit builtin function when multiple minus & plus

--- src_bonus/built_in/exit_bonus.c
+++ src_bonus/built_in/exit_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jiyunpar <jiyunpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 13:53:25 by jiyunpar          #+#    #+#             */
-/*   Updated: 2022/12/23 17:12:38 by junji            ###   ########.fr       */
+/*   Updated: 2022/12/26 15:11:52 by jiyunpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 // exit
 // - when exit built-in execute in child process, don't print "exit" on terminal
 // - argument > INT = undefined behavior;
+
 static bool	command_has_alpha(char *command)
 {
 	int	i;
@@ -22,9 +23,11 @@ static bool	command_has_alpha(char *command)
 	i = 0;
 	if (command == NULL)
 		return (false);
+	if (command[0] == '-' || command[0] == '+')
+		++i;
 	while (command[i])
 	{
-		if (ft_isalpha(command[i]))
+		if (!ft_isdigit(command[i]))
 			return (true);
 		++i;
 	}
@@ -37,20 +40,22 @@ int	b_exit(char **command, bool parent)
 
 	if (parent)
 		ft_putendl_fd("exit", 2);
+	if (ft_strcmp(command[1], "--") == 0)
+		exit(0);
 	i = 0;
 	while (command[i])
 		i++;
-	if (command_has_alpha(command[1]) == true)
-	{
-		ft_putstr_fd("exit: ", 2);
-		ft_putendl_fd("numeric argument required", 2);
-		exit(2);
-	}
 	if (i > 2)
 	{
 		ft_putstr_fd("exit: ", 2);
 		ft_putendl_fd("Too many arguments", 2);
 		return (1);
+	}
+	if (command_has_alpha(command[1]) == true)
+	{
+		ft_putstr_fd("exit: ", 2);
+		ft_putendl_fd("numeric argument required", 2);
+		exit(2);
 	}
 	if (command[1] == NULL)
 		exit(0);
